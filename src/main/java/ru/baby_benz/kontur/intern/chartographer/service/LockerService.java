ae58f9d84ba171d684f8108d1b816796f81293b2
Nikita <baby0benz@gmail.com>
2022-03-29 01:21:13 +0300

    use static duration instead of @Value property remove unused addAndAcquireLock

--- src/main/java/ru/baby_benz/kontur/intern/chartographer/service/LockerService.java
+++ src/main/java/ru/baby_benz/kontur/intern/chartographer/service/LockerService.java
@@ -5,8 +5,6 @@ import ru.baby_benz.kontur.intern.chartographer.service.impl.LockType;
 public interface LockerService {
     void addLock(String id);
 
-    void addAndAcquireLock(String id, LockType lockType);
-
     boolean acquireLock(String id, LockType lockType) throws InterruptedException;
 
     void freeLock(String id, LockType lockType);
--- src/main/java/ru/baby_benz/kontur/intern/chartographer/service/impl/ReadWriteLockerService.java
+++ src/main/java/ru/baby_benz/kontur/intern/chartographer/service/impl/ReadWriteLockerService.java
@@ -1,10 +1,10 @@
 package ru.baby_benz.kontur.intern.chartographer.service.impl;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.baby_benz.kontur.intern.chartographer.controller.exception.ChartaNotFoundException;
 import ru.baby_benz.kontur.intern.chartographer.service.LockerService;
 
+import java.time.Duration;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
@@ -14,8 +14,7 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 @Service
 public class ReadWriteLockerService implements LockerService {
     private final ConcurrentMap<String, ReadWriteLock> lockMap = new ConcurrentHashMap<>();
-    @Value("${service.image.lock.trying-time}")
-    private long tryingTime;
+    private static final Duration TRYING_SECONDS_DURATION = Duration.ofSeconds(10);
 
     @Override
     public void addLock(String id) {
@@ -23,28 +22,13 @@ public class ReadWriteLockerService implements LockerService {
     }
 
     @Override
-    public void addAndAcquireLock(String id, LockType lockType) {
-        lockMap.computeIfAbsent(id, v -> {
-            ReadWriteLock lock = new ReentrantReadWriteLock();
-            if (lockType.equals(LockType.SHARED)) {
-                lock.readLock().lock();
-            } else if (lockType.equals(LockType.EXCLUSIVE)) {
-                lock.writeLock().lock();
-            } else {
-                throw new IllegalArgumentException();
-            }
-            return lock;
-        });
-    }
-
-    @Override
     public boolean acquireLock(String id, LockType lockType) throws InterruptedException {
         ReadWriteLock lock = lockMap.get(id);
         if (lock != null) {
             if (lockType.equals(LockType.SHARED)) {
-                return lock.readLock().tryLock(tryingTime, TimeUnit.SECONDS);
+                return lock.readLock().tryLock(TRYING_SECONDS_DURATION.getSeconds(), TimeUnit.SECONDS);
             } else if (lockType.equals(LockType.EXCLUSIVE)) {
-                return lock.writeLock().tryLock(tryingTime, TimeUnit.SECONDS);
+                return lock.writeLock().tryLock(TRYING_SECONDS_DURATION.getSeconds(), TimeUnit.SECONDS);
             } else {
                 throw new IllegalArgumentException();
             }
