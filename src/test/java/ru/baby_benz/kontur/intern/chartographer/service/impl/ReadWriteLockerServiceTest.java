71fb80f5923fc7a8af56f5830fe01883dabc5b0b
Nikita <baby0benz@gmail.com>
2022-03-29 13:29:56 +0300

    fully cover ReadWriteLockerService with unit tests

--- src/test/java/ru/baby_benz/kontur/intern/chartographer/service/impl/ReadWriteLockerServiceTest.java
+++ src/test/java/ru/baby_benz/kontur/intern/chartographer/service/impl/ReadWriteLockerServiceTest.java
@@ -23,43 +23,31 @@ public class ReadWriteLockerServiceTest {
     }
 
     @Test
-    public void givenRandomId_whenAddLock_thenNoExceptionIsThrown() {
+    public void givenMockedId_whenAddLock_thenNoExceptionIsThrown() {
         assertDoesNotThrow(() -> lockerService.addLock(Mockito.anyString()));
     }
 
     @Test
-    public void givenRandomId_whenAddLockAndAcquireExclusiveLock_thenTrueReturned() throws InterruptedException {
+    public void givenMockedIdAndExclusiveLockType_whenAddLockAndAcquireLock_thenTrueReturned() throws InterruptedException {
         String id = Mockito.anyString();
-        LockType lockType = LockType.EXCLUSIVE;
         lockerService.addLock(id);
-        assertTrue(lockerService.acquireLock(id, lockType));
+        assertTrue(lockerService.acquireLock(id, LockType.EXCLUSIVE));
     }
 
     @Test
-    public void givenRandomId_whenAddLockAndAcquireSharedLock_thenTrueReturned() throws InterruptedException {
+    public void givenMockedIdAndSharedLockType_whenAddLockAndAcquireLock_thenTrueReturned() throws InterruptedException {
         String id = Mockito.anyString();
-        LockType lockType = LockType.SHARED;
         lockerService.addLock(id);
-        assertTrue(lockerService.acquireLock(id, lockType));
+        assertTrue(lockerService.acquireLock(id, LockType.SHARED));
     }
 
     @Test
-    public void givenRandomId_whenAddLockAndAcquireInvalidLock_thenExceptionIsThrown() {
-        String id = Mockito.anyString();
-        lockerService.addLock(id);
-        assertThrows(IllegalArgumentException.class, () -> lockerService.acquireLock(
-                id,
-                LockType.valueOf(LockType.class, "Invalid"))
-        );
-    }
-
-    @Test
-    public void givenRandomId_whenAddAndAcquireExclusiveLock_thenExceptionIsThrown() {
+    public void givenMockedIdAndMockedLockType_whenAddLockAndAcquireLock_thenExceptionIsThrown() {
         assertThrows(ChartaNotFoundException.class, () -> lockerService.acquireLock(Mockito.anyString(), Mockito.any()));
     }
 
     @Test
-    public void givenRandomId_whenAddAndAcquireExclusiveLockInSeparateThread_thenFalseReturned() throws InterruptedException, ExecutionException {
+    public void givenMockedIdAndExclusiveLockType_whenAddLockAndAcquireLockInSeparateThread_thenFalseReturned() throws InterruptedException, ExecutionException {
         String id = Mockito.anyString();
         LockType lockType = LockType.EXCLUSIVE;
 
@@ -71,7 +59,7 @@ public class ReadWriteLockerServiceTest {
     }
 
     @Test
-    public void givenRandomId_whenAddAndAcquireSharedLockInSeparateThread_thenTrueReturned() throws InterruptedException, ExecutionException {
+    public void givenMockedIdAndSharedLockType_whenAddLockAndAcquireLockInSeparateThread_thenTrueReturned() throws InterruptedException, ExecutionException {
         String id = Mockito.anyString();
         LockType lockType = LockType.SHARED;
 
@@ -80,6 +68,131 @@ public class ReadWriteLockerServiceTest {
 
         Future<Boolean> future = Executors.newSingleThreadExecutor().submit(new AcquireLockTask(id, lockType));
         assertTrue(future.get());
+    }
+
+    @Test
+    public void givenMockedIdAndExclusiveLockType_whenAddLockAndAcquireSharedLockInSeparateThread_thenFalseReturned() throws InterruptedException, ExecutionException {
+        String id = Mockito.anyString();
+
+        lockerService.addLock(id);
+        assertTrue(lockerService.acquireLock(id, LockType.EXCLUSIVE));
+
+        Future<Boolean> future = Executors.newSingleThreadExecutor().submit(new AcquireLockTask(id, LockType.SHARED));
+        assertFalse(future.get());
+    }
+
+    @Test
+    public void givenMockedIdAndSharedLockType_whenAddLockAndAcquireExclusiveLockInSeparateThread_thenFalseReturned() throws InterruptedException, ExecutionException {
+        String id = Mockito.anyString();
+
+        lockerService.addLock(id);
+        assertTrue(lockerService.acquireLock(id, LockType.SHARED));
+
+        Future<Boolean> future = Executors.newSingleThreadExecutor().submit(new AcquireLockTask(id, LockType.EXCLUSIVE));
+        assertFalse(future.get());
+    }
+
+    @Test
+    public void givenMockedIdAndMockedLockType_whenFreeLock_thenNoExceptionIsThrown() {
+        assertDoesNotThrow(() -> lockerService.freeLock(Mockito.anyString(), Mockito.any()));
+    }
+
+    @Test
+    public void givenMockedIdAndExclusiveLockType_whenAddLockAndFreeLock_thenNoExceptionIsThrown() {
+        String id = Mockito.anyString();
+        lockerService.addLock(id);
+        assertDoesNotThrow(() -> lockerService.freeLock(id, LockType.EXCLUSIVE));
+    }
+
+    @Test
+    public void givenMockedIdAndSharedLockType_whenAddLockAndFreeLock_thenNoExceptionIsThrown() {
+        String id = Mockito.anyString();
+        lockerService.addLock(id);
+        assertDoesNotThrow(() -> lockerService.freeLock(id, LockType.EXCLUSIVE));
+    }
+
+    @Test
+    public void givenMockedIdAndExclusiveLockType_whenAddLockAndAcquireLockAndFreeLock_thenNoExceptionIsThrown() throws InterruptedException {
+        String id = Mockito.anyString();
+        LockType lockType = LockType.EXCLUSIVE;
+
+        lockerService.addLock(id);
+        assertTrue(lockerService.acquireLock(id, lockType));
+        assertDoesNotThrow(() -> lockerService.freeLock(id, lockType));
+    }
+
+    @Test
+    public void givenMockedIdAndSharedLockType_whenAddLockAndAcquireLockAndFreeLock_thenNoExceptionIsThrown() throws InterruptedException {
+        String id = Mockito.anyString();
+        LockType lockType = LockType.SHARED;
+
+        lockerService.addLock(id);
+        assertTrue(lockerService.acquireLock(id, lockType));
+        assertDoesNotThrow(() -> lockerService.freeLock(id, lockType));
+    }
+
+    @Test
+    public void givenMockedIdAndExclusiveLockType_whenAddLockAndAcquireLockInSeparateThreadAndFreeLock_thenTrueReturned() throws InterruptedException, ExecutionException {
+        String id = Mockito.anyString();
+        LockType lockType = LockType.EXCLUSIVE;
+
+        lockerService.addLock(id);
+        assertTrue(lockerService.acquireLock(id, lockType));
+
+        Future<Boolean> future = Executors.newSingleThreadExecutor().submit(new AcquireLockTask(id, lockType));
+        lockerService.freeLock(id, lockType);
+        assertTrue(future.get());
+    }
+
+    @Test
+    public void givenMockedIdAndSharedLockType_whenAddLockAndAcquireLockInSeparateThreadAndFreeLock_thenTrueReturned() throws InterruptedException, ExecutionException {
+        String id = Mockito.anyString();
+        LockType lockType = LockType.SHARED;
+
+        lockerService.addLock(id);
+        assertTrue(lockerService.acquireLock(id, lockType));
+
+        Future<Boolean> future = Executors.newSingleThreadExecutor().submit(new AcquireLockTask(id, lockType));
+        lockerService.freeLock(id, lockType);
+        assertTrue(future.get());
+    }
+
+    @Test
+    public void givenMockedIdAndExclusiveLockType_whenAddLockAndAcquireSharedLockInSeparateThreadAndFreeLock_thenTrueReturned() throws InterruptedException, ExecutionException {
+        String id = Mockito.anyString();
+        LockType currentThreadLockType = LockType.EXCLUSIVE;
+
+        lockerService.addLock(id);
+        assertTrue(lockerService.acquireLock(id, currentThreadLockType));
+
+        Future<Boolean> future = Executors.newSingleThreadExecutor().submit(new AcquireLockTask(id, LockType.SHARED));
+        lockerService.freeLock(id, currentThreadLockType);
+        assertTrue(future.get());
+    }
+
+    @Test
+    public void givenMockedIdAndSharedLockType_whenAddLockAndAcquireExclusiveLockInSeparateThreadAndFreeLock_thenFalseReturned() throws InterruptedException, ExecutionException {
+        String id = Mockito.anyString();
+        LockType currentThreadLockType = LockType.SHARED;
+
+        lockerService.addLock(id);
+        assertTrue(lockerService.acquireLock(id, currentThreadLockType));
+
+        Future<Boolean> future = Executors.newSingleThreadExecutor().submit(new AcquireLockTask(id, LockType.EXCLUSIVE));
+        lockerService.freeLock(id, currentThreadLockType);
+        assertTrue(future.get());
+    }
+
+    @Test
+    public void givenMockedId_whenRemoveLock_thenNoExceptionIsThrown() {
+        assertDoesNotThrow(() -> lockerService.removeLock(Mockito.anyString()));
+    }
+
+    @Test
+    public void givenMockedId_whenAddLockAndRemoveLock_thenNoExceptionIsThrown() {
+        String id = Mockito.anyString();
+        lockerService.addLock(id);
+        assertDoesNotThrow(() -> lockerService.removeLock(id));
     }
 
     @RequiredArgsConstructor
